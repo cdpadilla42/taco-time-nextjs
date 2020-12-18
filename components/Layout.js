db91369276c838e3966dfdfea468d5a1b73cee6d
Chris Padilla <cdpadilla42@gmail.com>
2020-12-18 16:21:15 -0600

    css reset and stylize customization heading

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,15 @@ const Layout = ({ children }) => {
     * {
       font-family: Inter, Helvetica, Arial, sans-serif;
     }
+
+    body {
+      margin: 0;
+    }
+
+    p, h1, h2, h3, h4, h5, h6 {
+      margin: 1rem 1rem;
+      padding: 0;
+    }
   `;
   return (
     <div className="layout">
--- pages/item/[itemID].js
+++ pages/item/[itemID].js
@@ -10,8 +10,15 @@ const StyledItemDetails = styled.div`
     width: 300;
     margin: 0 auto;
   }
-  h2 {
-    color: green;
+
+  .options .title {
+    background-color: rgba(0, 0, 0, 0.05);
+    padding: 0.6rem;
+    margin: 0;
+
+    h2 {
+      margin: 1rem 0.5rem;
+    }
   }
 `;
 
@@ -25,13 +32,16 @@ const itemDisplay = () => {
       <div className="container">
         <img src={testItem.img} alt="" />
         <h2>{testItem.name}</h2>
+        <p>$1,000,000</p>
         <p>{testItem.description}</p>
       </div>
       <div className="options">
         {testItem.customizations.map((customizeable) => {
           return (
             <>
-              <h3>{customizeable.title}</h3>
+              <div className="title">
+                <h2>{customizeable.title}</h2>
+              </div>
               {customizeable.options.map((option) => {
                 return <p>{option.name}</p>;
               })}
