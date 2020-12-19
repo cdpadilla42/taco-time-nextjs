6dc43a9a51a103a106d943231f3d61939d22ecb0
Chris Padilla <cdpadilla42@gmail.com>
2020-12-19 13:18:39 -0600

    Connect to mongoDB

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { schema } from '../../apollo/schema';
+import connectDb from '../../lib/mongoose';
 
 const apolloServer = new ApolloServer({ schema });
 
@@ -9,4 +10,5 @@ export const config = {
   },
 };
 
-export default apolloServer.createHandler({ path: '/api/graphql' });
+const server = apolloServer.createHandler({ path: '/api/graphql' });
+export default connectDb(server);
