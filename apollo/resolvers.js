6a4ff96e7527c4967c25399ac1ec9782ffcf1637
Chris Padilla <cdpadilla42@gmail.com>
2020-12-20 17:44:40 -0600

    Add item creation to resolvers function

--- apollo/resolvers.js
+++ apollo/resolvers.js
@@ -1,3 +1,5 @@
+import { Item } from './item';
+
 export const resolvers = {
   Query: {
     viewer(_parent, _args, _context, _info) {
@@ -10,6 +12,19 @@ export const resolvers = {
   },
   Mutation: {
     addItem(_, args) {
+      const newItem = new Item({
+        name: 'Test',
+        price: 1000,
+        img: 'cool',
+      });
+
+      newItem.save((err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(result);
+        }
+      });
       console.log(args);
       return args.input;
     },
