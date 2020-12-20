a5374f8ce62185a3d6d956a16339a9eaffec99b7
Chris Padilla <cdpadilla42@gmail.com>
2020-12-20 17:40:06 -0600

    Write mongoose schemas for items

---
+++ apollo/item.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from 'mongoose';
+
+const OptionsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: Number,
+});
+
+const CustomizationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    required: true,
+  },
+  options: [OptionsSchema],
+});
+
+const ItemSchema = new Schema({
+  img: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  customizations: [CustomizationSchema],
+});
+
+export const Item =
+  mongoose.models.items || mongoose.model('items', ItemSchema);
