5357534550ca2d6ab085b32892e13027246f534f
Chris Padilla <cdpadilla42@gmail.com>
2020-12-20 22:20:14 -0600

    add gql query to dynamic item page

--- pages/item/[itemID].js
+++ pages/item/[itemID].js
@@ -2,6 +2,15 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { dummyData } from '../../lib/dummyData';
 import styled from 'styled-components';
+import gql from 'graphql-tag';
+
+const ItemByIdQuery = gql`
+  query getItem($id: ID!) {
+    itemById(id: $id) {
+      name
+    }
+  }
+`;
 
 const StyledItemDetails = styled.div`
   img {
@@ -81,5 +90,32 @@ const itemDisplay = () => {
     </StyledItemDetails>
   );
 };
+
+// // This function gets called at build time
+// export async function getStaticPaths() {
+//   // Call an external API endpoint to get posts
+//   const res = await fetch('https://.../posts')
+//   const posts = await res.json()
+
+//   // Get the paths we want to pre-render based on posts
+//   const paths = posts.map((post) => ({
+//     params: { id: post.id },
+//   }))
+
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: false } means other routes should 404.
+//   return { paths, fallback: false }
+// }
+
+// // This also gets called at build time
+// export async function getStaticProps({ params }) {
+//   // params contains the post `id`.
+//   // If the route is like /posts/1, then params.id is 1
+//   const res = await fetch(`https://.../posts/${params.id}`)
+//   const post = await res.json()
+
+//   // Pass post data to the page via props
+//   return { props: { post } }
+// }
 
 export default itemDisplay;
