7b4a3966545e93c4c1ce00df02dd614c08cee8a2
Chris Padilla <cdpadilla42@gmail.com>
2020-12-24 12:20:39 -0600

    Add redux support to application

---
+++ components/Cart.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cart = () => {
+  return <div>🛒</div>;
+};
+
+export default Cart;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../apollo/client';
 import Layout from '../components/Layout';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from '../lib/redux';
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ReduxProvider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ReduxProvider>
     </ApolloProvider>
   );
 }
