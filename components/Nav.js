6ad69d92ed3fb6c6cc0908f11c8f01864a27e5c3
Chris Padilla <cdpadilla42@gmail.com>
2020-12-28 11:52:24 -0600

    Add css animation to cart toaster

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleCart } from '../lib/redux';
+import styled from 'styled-components';
+import wait from 'waait';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const StyledNav = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+
+  .logo {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+  }
+
+  .home,
+  button {
+    flex-basis: 3rem;
+  }
+
+  .toast {
+    position: absolute;
+    right: 0.5rem;
+    top: 2.25rem;
+    background-color: #2c7d3d;
+    color: #fffbfa;
+    padding: 0.6rem;
+    border-radius: 5px;
+  }
+
+  .toaster-enter {
+    transform: translateY(-20%);
+    transition: transform 0.5s, color 0.5s, background-color 0.5s;
+    background-color: rgba(0, 0, 0, 0);
+    color: rgba(0, 0, 0, 0);
+    &.toaster-enter-active {
+      transform: translateY(0);
+      background-color: #2c7d3d;
+      color: #fffbfa;
+    }
+  }
+
+  .toaster-exit {
+    transform: translateY(0);
+    transition: transform 0.5s, color 0.5s, background-color 0.5s;
+    transform: translateY(0);
+    background-color: #2c7d3d;
+    color: #fffbfa;
+    &.toaster-exit-active {
+      transform: translateY(-20%);
+      background-color: rgba(0, 0, 0, 0);
+      color: rgba(0, 0, 0, 0);
+    }
+  }
+`;
 
 const Nav = () => {
   const dispatch = useDispatch();
+  const [isToastShowing, setIsToastShowing] = useState(false);
 
   function handleClick() {
     dispatch(toggleCart());
   }
+
+  async function showToast() {
+    console.log('toast on');
+    setIsToastShowing(true);
+    await wait(4000);
+    console.log('bye toast');
+    setIsToastShowing(false);
+  }
+
   return (
-    <div>
-      <button onClick={handleClick} style={{ position: 'absolute', right: 0 }}>
-        Cart
-      </button>
-    </div>
+    <StyledNav>
+      <div className="home">House</div>
+      <div className="logo">Taco Time!</div>
+      <button onClick={showToast}>Toast</button>
+      <button onClick={handleClick}>Cart</button>
+      <TransitionGroup component="div">
+        {isToastShowing && (
+          <CSSTransition
+            classNames="toaster"
+            key="toaster"
+            timeout={{ enter: 500, exit: 500 }}
+          >
+            <div className="toast">Toast time!</div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </StyledNav>
   );
 };
 
