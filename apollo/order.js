3e930f5f601719f00e0b1f7e7bf7a073457f1308
Chris Padilla <cdpadilla42@gmail.com>
2021-02-04 11:36:36 -0600

    Write MongoDB Schemas for Order and OrderItems

---
+++ apollo/order.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+
+const OrderItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const OrderSchema = new Schema({
+  items: [OrderItemSchema],
+  total: {
+    type: Number,
+    required: true,
+  },
+  charge: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Order =
+  mongoose.models.orders || mongoose.model('orders', OrderSchema);
