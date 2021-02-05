411a6ec22b5e01c3628b9ed744892f16523bf615
Chris Padilla <cdpadilla42@gmail.com>
2021-02-05 09:28:44 -0600

    Return to previous sc ssr implementation

--- pages/_document.js
+++ pages/_document.js
@@ -11,41 +11,41 @@ export default class MyDocument extends Document {
     return { ...page, styleTags };
   }
 
-  // static async getServerSideProps(ctx) {
-  //   const sheet = new ServerStyleSheet();
-  //   const originalRenderPage = ctx.renderPage;
+  static async getServerSideProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  //   try {
-  //     ctx.renderPage = () =>
-  //       originalRenderPage({
-  //         enhanceApp: (App) => (props) =>
-  //           sheet.collectStyles(<App {...props} />),
-  //       });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-  //     const initialProps = await Document.getServerSideProps(ctx);
-  //     return {
-  //       ...initialProps,
-  //       styles: (
-  //         <>
-  //           {initialProps.styles}
-  //           {sheet.getStyleElement()}
-  //         </>
-  //       ),
-  //     };
-  //   } finally {
-  //     sheet.seal();
-  //   }
-  // }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+      const initialProps = await Document.getServerSideProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
+
+  // render() {
+  //   return (
+  //     <Html>
+  //       <Head />
+  //       <body>
+  //         <Main />
+  //         <NextScript />
+  //       </body>
+  //     </Html>
+  //   );
+  // }
 }
