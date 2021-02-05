5b9378b96e921077f3b30d0477ac73c61f95152a
Chris Padilla <cdpadilla42@gmail.com>
2021-02-05 09:09:36 -0600

    Load styles on the server

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,51 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getServerSideProps(ctx) {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+  // static async getServerSideProps(ctx) {
+  //   const sheet = new ServerStyleSheet();
+  //   const originalRenderPage = ctx.renderPage;
 
-      const initialProps = await Document.getServerSideProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+  //   try {
+  //     ctx.renderPage = () =>
+  //       originalRenderPage({
+  //         enhanceApp: (App) => (props) =>
+  //           sheet.collectStyles(<App {...props} />),
+  //       });
+
+  //     const initialProps = await Document.getServerSideProps(ctx);
+  //     return {
+  //       ...initialProps,
+  //       styles: (
+  //         <>
+  //           {initialProps.styles}
+  //           {sheet.getStyleElement()}
+  //         </>
+  //       ),
+  //     };
+  //   } finally {
+  //     sheet.seal();
+  //   }
+  // }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
